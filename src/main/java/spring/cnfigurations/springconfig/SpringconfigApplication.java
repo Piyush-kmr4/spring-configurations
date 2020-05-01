cb79ea621a8d54f5ed4233990e838eeaf0c9dfe9
Piyush-kmr4 <piyushkmr014@gmail.com>
2020-05-01 19:25:41 +0530

    XML Based Configuration done in the chuck-config.xml file

--- src/main/java/spring/cnfigurations/springconfig/SpringconfigApplication.java
+++ src/main/java/spring/cnfigurations/springconfig/SpringconfigApplication.java
@@ -2,8 +2,10 @@ package spring.cnfigurations.springconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringconfigApplication {
 
     public static void main(String[] args) {
